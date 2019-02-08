d986a8fe9b23fc9436ba6b3b8fe59af6409861da
Frey Hargarten <harga012@umn.edu>
2019-02-08 16:24:47 -0600

    updates

--- app/bigmap.js
+++ app/bigmap.js
@@ -39,22 +39,22 @@ class BigMap {
             .style("stroke", "#ffffff")
             .style("fill", function(d){
                 if (self.race == 0) {
-                    if (d.properties.STUSPS == "OR" || d.properties.STUSPS == "WI" || d.properties.STUSPS == "IL" || d.properties.STUSPS == "PA") { return "#0258a0"; }
+                    if (d.properties.STUSPS == "OR" || d.properties.STUSPS == "WI" || d.properties.STUSPS == "IL" || d.properties.STUSPS == "PA" || d.properties.STUSPS == "NJ" || d.properties.STUSPS == "MA") { return "#0258a0"; }
                     else if (d.properties.STUSPS == "CA" || d.properties.STUSPS == "SD" || d.properties.STUSPS == "NE" || d.properties.STUSPS == "IN") { return "#857AAA"; }
                     else if (d.properties.STUSPS == "OH") { return "#7F98AA"; }
                     else if (d.properties.STUSPS == "NH") { return "#A7E6E3"; }
                     else if (d.properties.STUSPS == "FL") { return "#5BBF48"; }
+                    else if (d.properties.STUSPS == "WV") { return "#999999"; }
                     else { return "#dddddd"; }
                 } else if (self.race == 1) {
-                    if (d.properties.STUSPS == "WA" || d.properties.STUSPS == "TX" || d.properties.STUSPS == "MN" || d.properties.STUSPS == "MI" || d.properties.STUSPS == "WV" || d.properties.STUSPS == "PA" || d.properties.STUSPS == "MD" || d.properties.STUSPS == "CT" || d.properties.STUSPS == "RI" || d.properties.STUSPS == "MA" || d.properties.STUSPS == "ME" || d.properties.STUSPS == "DC") { return "#0258a0"; }
-                    else if (d.properties.STUSPS == "AR" || d.properties.STUSPS == "LA" || d.properties.STUSPS == "MS" || d.properties.STUSPS == "AL" || d.properties.STUSPS == "GA") { return "#5BBF48"; }
+                    if (d.properties.STUSPS == "CT" || d.properties.STUSPS == "DC" || d.properties.STUSPS == "HI" || d.properties.STUSPS == "ME" || d.properties.STUSPS == "MD" || d.properties.STUSPS == "MA" || d.properties.STUSPS == "MI" || d.properties.STUSPS == "MN" || d.properties.STUSPS == "NY" || d.properties.STUSPS == "PA" || d.properties.STUSPS == "RI" || d.properties.STUSPS == "TX" || d.properties.STUSPS == "WA"  || d.properties.STUSPS == "WV") { return "#0258a0"; }
+                    else if (d.properties.STUSPS == "AL" || d.properties.STUSPS == "AR" || d.properties.STUSPS == "GA" || d.properties.STUSPS == "LA" || d.properties.STUSPS == "MS") { return "#5BBF48"; }
                     else { return "#c0272d"; }
                 } else if (self.race == 2) {
                     if (d.properties.STUSPS == "MN" || d.properties.STUSPS == "DC") { return "#0258a0"; }
                     else { return "#c0272d"; }
                 } else if (self.race == 3) {
-                    if (d.properties.STUSPS == "ND" || d.properties.STUSPS == "KS" || d.properties.STUSPS == "OK" || d.properties.STUSPS == "AL" || d.properties.STUSPS == "MS" || d.properties.STUSPS == "TN") { return "#D4CB6A"; }
-                    else if (d.properties.STUSPS == "MN" || d.properties.STUSPS == "IA" || d.properties.STUSPS == "LA" || d.properties.STUSPS == "ME") { return "#F2D2A4"; }
+                    if (d.properties.STUSPS == "ND" || d.properties.STUSPS == "MN" || d.properties.STUSPS == "IA" || d.properties.STUSPS == "MO" || d.properties.STUSPS == "CO" || d.properties.STUSPS == "KS" || d.properties.STUSPS == "OK" || d.properties.STUSPS == "LA" || d.properties.STUSPS == "AL" || d.properties.STUSPS == "MS" || d.properties.STUSPS == "TN") { return "#D4CB6A"; }
                     else if (d.properties.STUSPS == "GA" || d.properties.STUSPS == "SC") { return "#F2614C"; }
                     else { return "#c0272d"; }
                 } else if (self.race == 4) {
